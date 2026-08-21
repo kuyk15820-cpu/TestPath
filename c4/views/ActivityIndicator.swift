1859c7dae82d81c902bfc910861f88fd04f0506f
kuyk15820-cpu <kuyk15820@gmail.com>
2026-08-22 01:12:19 +0700

    Update ActivityIndicator.swift

--- c4/views/ActivityIndicator.swift
+++ c4/views/ActivityIndicator.swift
@@ -1,1 +1,22 @@
-1
+import SwiftUI
+import UIKit
+
+// MARK: - UIActivityIndicatorView Wrapper
+struct ActivityIndicator: UIViewRepresentable {
+    var isAnimating: Bool = true
+    var style: UIActivityIndicatorView.Style = .medium
+
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let indicator = UIActivityIndicatorView(style: style)
+        indicator.hidesWhenStopped = true
+        return indicator
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        if isAnimating {
+            uiView.startAnimating()
+        } else {
+            uiView.stopAnimating()
+        }
+    }
+}
